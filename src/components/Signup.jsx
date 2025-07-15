24207652abf47494537aeffcc410181dd056761a
trnth <trannhuthuan2004@gmail.com>
2025-07-15 17:33:43 +0700

    add Login, SignUp

---
+++ src/components/Signup.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { toast } from "sonner";
+const Signin = () => {
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const singUpHandler = async (e) => {
+    console.log(input);
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        "http://localhost:5000/api/v1/user/register",
+        input,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        toast.success(res.data.message);
+        setInput({ ...input, [e.target.name]: e.target.value });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.responese.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <div className="flex items-center w-screen h-screen justify-center">
+        <form
+          className="w-full max-w-sm rounded-2xl bg-white shadow-lg flex flex-col gap-1 p-4"
+          onSubmit={singUpHandler}
+          method="post"
+        >
+          <div className="my-4">
+            <h1 className="text-center font-bold text-2xl">Flowfy</h1>
+            <p className="text-center italic text-sm">
+              Signup to "flow" with your friends
+            </p>
+          </div>
+          <div>
+            <span className="font-medium">Username</span>
+            <Input
+              type="text"
+              name="username"
+              value={input.username}
+              onChange={changeEventHandler}
+              className="focus-visible:ring-transparent my-2"
+            />
+          </div>
+          <div>
+            <span className="font-medium">Email</span>
+            <Input
+              type="email"
+              name="email"
+              value={input.email}
+              onChange={changeEventHandler}
+              className="focus-visible:ring-transparent my-2"
+            />
+          </div>
+          <div>
+            <span className="font-medium">Password</span>
+            <Input
+              type="password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
+              className="focus-visible:ring-transparent my-2"
+            />
+          </div>
+
+          <Button type="submit" className="mt-4 w-full">
+            Signup
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
